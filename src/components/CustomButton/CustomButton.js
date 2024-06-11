1bae0b72468da3614e9d8d04db6e25c92c5ef282
Misha Hrysiuk <0973853103h@gmail.com>
2024-06-11 18:36:46 +0300

    add  TelegramUI

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Button } from "@mui/material";
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 import { useTelegram } from "../../hooks/useTelegram";
+import { Button } from "@telegram-apps/telegram-ui";
 
 const CustomButton = (props) => {
     const { themeParams } = useTelegram();
 
     console.log(themeParams);
 
-    const CustomButton = styled(Button)({
-        color: themeParams?.button_text_color || "#55FF55",
-        backgroundColor: themeParams?.button_color,
-    });
+    // const CustomButton = styled(Button)({
+    //     color: themeParams?.button_text_color || "#55FF55",
+    //     backgroundColor: themeParams?.button_color,
+    // });
 
-    return <CustomButton {...props} />;
+    return <Button {...props} mode="filled" size="s" />;
 };
 
 export default CustomButton;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <AppRoot>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AppRoot>
     </React.StrictMode>,
 );
 
